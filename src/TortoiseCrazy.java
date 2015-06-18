80aff1848b3668738bfbab8271f10ae3b1055c14
betsylouj <Guest@wts12.attlocal.net>
2015-06-18 16:55:36 -0700

    Blah

--- src/TortoiseCrazy.java
+++ src/TortoiseCrazy.java
@@ -1,6 +1,6 @@
-import org.teachingextensions.logo.Colors;
+import org.teachingextensions.logo.PenColors;
 import org.teachingextensions.logo.Tortoise;
-/* Teacher’s note: before beginning, draw a pentagon and have students work out the angle that the tortoise will have to turn (360/5) */
+/* Teacherï¿½s note: before beginning, draw a pentagon and have students work out the angle that the tortoise will have to turn (360/5) */
 
 public class TortoiseCrazy
 {
@@ -11,15 +11,16 @@ public class TortoiseCrazy
 
  private void makePrettyThings()
  {
+	 Tortoise.setPenColor(PenColors.getRandomColor());
      // 6. make the tortoise go at maximum speed
 Tortoise.setSpeed(10);
 
      // 9. choose a color that you like for the shape
-Tortoise.setPenColor(Colors.Reds.Crimson);
+Tortoise.setPenColor(PenColors.Reds.Crimson);
  
-     // 2. make a variable for the number of sides you want (can’t test this one)
+     // 2. make a variable for the number of sides you want (canï¿½t test this one)
 int sides = 5;
-     // 3. make a variable for the angle you want the tortoise to turn. Hint: you can divide in Java using “/”.  Can’t test until step 5.
+     // 3. make a variable for the angle you want the tortoise to turn. Hint: you can divide in Java using ï¿½/ï¿½.  Canï¿½t test until step 5.
 int angle = 360/sides;
 int width = 0;
      // 5. Do everything below 200 times. When this is done you will see a pentagon.
@@ -27,10 +28,10 @@ int width = 0;
    {
          // 1. move the tortoise 100 pixels
     Tortoise.move(i);
-         // 7. make the tortoise move "i" pixels instead of 100 (don’t need new line of code for this, just change previous one)
+         // 7. make the tortoise move "i" pixels instead of 100 (donï¿½t need new line of code for this, just change previous one)
          // 4. turn the tortoise the amount in your angle variable
       Tortoise.turn(angle);   
-      Tortoise.setPenColor(Colors.getRandomColor());
+      Tortoise.setPenColor(PenColors.getRandomColor());
          // 8. turn the tortoise one more degree
       Tortoise.turn(1); 
       width = i/60 + 1;
