7084ab779d0a055651dae6b0cf34608c33517ed5
betsylouj <betsylouj@gmail.com>
2015-06-30 16:56:10 -0700

    Progress

---
+++ src/WorldsMostAnnoyingJoke.java
@@ -0,0 +1,18 @@
+import javax.swing.JOptionPane;
+
+
+public class WorldsMostAnnoyingJoke {
+	
+	public static void main(String[] args) {
+		
+		for (int i=0;i<3;i++)
+		{
+		JOptionPane.showInputDialog("Knock, Knock");
+		JOptionPane.showInputDialog("Banana");
+		}
+		JOptionPane.showInputDialog("Knock, Knock");
+		JOptionPane.showInputDialog("Orange");
+		JOptionPane.showMessageDialog(null, "Orange you glad it's not a banana!!!");
+	}
+
+}
