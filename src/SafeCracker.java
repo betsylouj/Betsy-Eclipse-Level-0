75606147a5ef8e739857df6efb4ff8aa833b0908
betsylouj <betsylouj@gmail.com>
2015-09-30 18:05:20 -0700

    Update

--- src/SafeCracker.java
+++ src/SafeCracker.java
@@ -11,7 +11,6 @@ public class SafeCracker {
         /* Your mission: use the tryCode method to crack the safe by trying all possible combinations */
 for (int i = 0; i < 100; i++) {
 	int myguess = 9999999 - i;
-	System.out.println(myguess);
 	tryCode(myguess);	
 }
        
