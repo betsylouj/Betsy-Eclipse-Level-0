8ec6f37f70462d0340f6ae1c58bcad5c9d02aaf8
betsylouj <betsylouj@gmail.com>
2015-09-30 18:02:52 -0700

    Safe cracker

---
+++ src/SafeCracker.java
@@ -0,0 +1,44 @@
+import java.net.URL;
+import java.util.Random;
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+import javax.swing.JOptionPane;
+
+public class SafeCracker {
+
+    public static void main(String[] args) {
+        /* Your mission: use the tryCode method to crack the safe by trying all possible combinations */
+for (int i = 0; i < 100; i++) {
+	int myguess = 9999999 - i;
+	System.out.println(myguess);
+	tryCode(myguess);	
+}
+       
+
+    }
+
+    static void tryCode(int guess) {
+        System.out.println("trying " + guess);
+        int secretCode = 9999999 - wekncrzpasfdkjhcfjse;
+        if (guess == secretCode) {
+            playSoundFromInternet("http://10.0.1.75/sounds/megusta.wav");
+            JOptionPane.showMessageDialog(null, "Congratulations! You cracked the safe with " + guess);
+            System.exit(0);
+        }
+    }
+
+    public static void playSoundFromInternet(String soundURL) {
+        try {
+            AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(new URL(soundURL));
+            Clip clip = AudioSystem.getClip();
+            clip.open(audioInputStream);
+            clip.start();
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    final static int wekncrzpasfdkjhcfjse = new Random().nextInt(100);
+}
+
