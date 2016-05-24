3171fe89c404292ff3841b8768bbeb7ba488b9a5
betsylouj <betsylouj@gmail.com>
2016-05-23 18:41:31 -0700

    Primes

--- src/Primes.java
+++ src/Primes.java
@@ -1,17 +1,17 @@
 public class Primes {
 	public static void main(String[] args) {
 		boolean prime;
-		
-		int numPrimes = 1; // 1 is prime
+
+		int numPrimes = 0; // 1 is not prime
 		for (int i = 2; i < 100; i++) {
 			prime = false;
-			System.out.println("i is " + i);
+			// System.out.println("i is " + i);
 			for (int j = 2; j < (Math.sqrt(i) + 1); j++) {
 				// System.out.println("j is " + j);
 				if (i % j == 0) {
 					if (i != j) {
 						prime = true;
-						System.out.println("Not prime ");
+						// System.out.println("Not prime ");
 					}
 				}
 			}
